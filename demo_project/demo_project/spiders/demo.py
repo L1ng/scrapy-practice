9eed3bec2f024b07b6bb39b42e986e856c680af5
L1ng <p.qingling@gmail.com>
2020-07-13 15:41:07 +0900

    add request cookies

--- demo_project/demo_project/spiders/demo.py
+++ demo_project/demo_project/spiders/demo.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.exception import CloseSpide
 import json
+import time
 
+# $ scrapy crawl demo -o data.json -a id ="test"
 class DemoSpider(scrapy.Spider):
     name = 'demo'
     allowed_domains = ['www.demo.com']
     url = 'http://www.demo.com/'
+    timestamp = int(time.time())
+    
+    cookies = {
+        'key': 'value'
+        'timestamp':timestamp
+    }
 
     def start_requests(self):
-        yield scrapy.Request(url=self.url, callback=self.parse_id)
+        yield scrapy.Request(url=self.url, callback=self.parse_id,
+                                cookies=self.cookies)
 
     def parse_id(self, response):
         data = json.loads(response.body)
@@ -17,9 +27,12 @@ class DemoSpider(scrapy.Spider):
             file.write(json.dumps(data))
 
         page_urls = data.get('key')
+        if page_urls is None:
+            raise CloseSpide("No result available in this id")
 
         for page_url in page_urls:
-            yield scrapy.Request(url='http://www.demo{0}.com'.format(id),callback=self.parse)
+            yield scrapy.Request(url='http://www.demo{0}.com'.format(self.id),callback=self.parse,
+                                cookies=self.cookies)
 
         pagination_metadata = data.get('pagination_key')
 
